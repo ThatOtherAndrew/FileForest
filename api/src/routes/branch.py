cbc93b5c40b1f9f8977c234cf48b499d7e5252fa
Andrew <stroev.andrew@gmail.com>
2022-09-14 20:51:34 +0100

    🗃️ Add CRUD for branches

--- api/src/routes/branch.py
+++ api/src/routes/branch.py
@@ -1,17 +1,46 @@
 from pathlib import Path
+from shutil import rmtree
 
-from fastapi import APIRouter, Response
-
+from fastapi import APIRouter, Response, HTTPException
 
 router = APIRouter(prefix='/branch')
+files = Path('files')
 
 
-@router.post('/new', status_code=201)
-async def create_branch(response: Response, name: str):
-    branch = Path('files') / name
+@router.post('/', status_code=201)
+def create(branch_id: str):
+    branch = files / branch_id
     if branch.is_dir():
-        response.status_code = 409
-        return {'success': False}
+        raise HTTPException(409, 'The specified branch ID already exists')
 
     branch.mkdir()
-    return {'success': True}
+    return Response()
+
+
+@router.get('/{branch_id}')
+def read(branch_id: str):
+    branch = files / branch_id
+    if not branch.is_dir():
+        raise HTTPException(404, 'The specified branch ID does not exist')
+
+    return branch.iterdir()
+
+
+@router.put('/{branch_id}', status_code=204)
+def update(branch_id: str, new_id: str):
+    branch = files / branch_id
+    if not branch.is_dir():
+        raise HTTPException(404, 'The specified branch ID does not exist')
+
+    branch.rename(files / new_id)
+    return Response()
+
+
+@router.delete('/{id}')
+def delete(branch_id: str):
+    branch = files / branch_id
+    if not branch.is_dir():
+        raise HTTPException(404, 'The specified branch ID does not exist')
+
+    rmtree(branch)
+    return Response()
