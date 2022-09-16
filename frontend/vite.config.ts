c544d4230f251866c786fe8f86811b51f8dfcc95
Andrew <stroev.andrew@gmail.com>
2022-09-16 22:58:40 +0100

    🔁 Fix HMR and networking

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    port: 80,
+    hmr: {
+      clientPort: 81
+    },
+    watch: {
+      usePolling: true
+    }
+  }
 };
 
 export default config;
